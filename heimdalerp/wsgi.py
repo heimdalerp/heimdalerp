27e8ea870f6bdc8db0037e6bdd149bbf3e3d7503
Mariano Baragiola <mariano@baragiola.com.ar>
2016-02-13 13:11:07 -0300

    FIX naming

--- heimdalerp/wsgi.py
+++ heimdalerp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for heimdallerp_ project.
+WSGI config for heimdalerp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heimdallerp_.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heimdalerp.settings")
 
 application = get_wsgi_application()
