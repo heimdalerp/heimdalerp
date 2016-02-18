04f5da028aaa85cf251430f65b4a1b43b3de82b4
mbaragiola <mariano@baragiola.com.ar>
2016-02-18 09:57:36 -0300

    ADD initial hr/serializers.py

---
+++ hr/serializers.py
@@ -0,0 +1,76 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+from hr import models
+
+
+class EthnicitySerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Ethnicity
+
+
+class SexualOrientationSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.SexualOrientation
+
+
+class AptitudeSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Aptitude
+
+
+class AchievementSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Achievement
+
+
+class SanctionSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Sanction
+
+
+class EmployeeSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Employee
+
+
+class LanguageSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Language
+
+
+class EmployeeSpeaksLanguageSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.EmployeeSpeaksLanguage
+
+
+class EmployeeHasSanctionSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.EmployeeHasSanction
+
+
+class AcademicInstitutionSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.AcademicInstitution
+
+
+class DegreeSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Degree
+
+
+class EmployeeHasDegreeSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Ethnicity
+
