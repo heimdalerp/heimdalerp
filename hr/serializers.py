67cd906410e699879f7c51376f5a11e3f2189f3e
mbaragiola <mariano@baragiola.com.ar>
2016-02-20 11:01:13 -0300

    IMP hr/serializers.py

--- hr/serializers.py
+++ hr/serializers.py
@@ -18,35 +18,93 @@ class EthnicitySerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Ethnicity
+        fields = (
+            'url',
+            'id',
+            'name',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:ethnicity-detail'
+            }
+        }
 
 
 class SexualOrientationSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.SexualOrientation
+        fields = (
+            'url',
+            'id',
+            'name',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:sexualorientation-detail'
+            }
+        }
 
 
 class AptitudeSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Aptitude
+        fields = (
+            'url',
+            'id',
+            'name',
+            'description',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:aptitude-detail'
+            }
+        }
 
 
 class AchievementSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Achievement
+        fields = (
+            'url',
+            'id',
+            'description',
+            'when_it_happened',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:achievement-detail'
+            }
+        }
 
 
 class SanctionSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Sanction
+        fields = (
+            'url',
+            'id',
+            'name',
+            'description',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:sanction-detail'
+            }
+        }
 
 
 class EmployeeSerializer(HyperlinkedModelSerializer):
-    user = UserSerializer()
-    born_in = CountrySerializer()
+    #user = UserSerializer()
+    #born_in = CountrySerializer()
     ## TODO: Decide if it is better to nest the relation or hyperlink it.
     #phone_numbers = persons_serializers.PhoneNumberSerializer()
     #extra_emails = persons_serializers.ExtraEmailSerializer()    
@@ -75,9 +133,9 @@ class EmployeeSerializer(HyperlinkedModelSerializer):
         fields = (
             'url',
             'id',
-            'user',
+            #'user',
             'birth_date',
-            'born_in',
+            #'born_in',
             'phone_numbers',
             'extra_emails',
             'genre',
@@ -107,34 +165,136 @@ class LanguageSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Language
+        fields = (
+            'url',
+            'id',
+            'name',
+            'points',
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:language-detail'
+            }
+        }
 
 
 class EmployeeSpeaksLanguageSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.EmployeeSpeaksLanguage
+        fields = (
+            'url',
+            'id',
+            'employee',
+            'language',
+            'level',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:employeespeakslanguage-detail'
+            },
+            'employee': {
+                'view_name': 'api:hr:employee-detail'
+            },
+            'language': {
+                'view_name': 'api:hr:language-detail'
+            }
+        }
 
 
 class EmployeeHasSanctionSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.EmployeeHasSanction
+        fields = (
+            'url',
+            'id',
+            'employee',
+            'sanction',
+            'what_happened',
+            'when_it_happened',
+            'others_implicated',
+            'victims'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:employeehassanction-detail'
+            },
+            'employee': {
+                'view_name': 'api:hr:employee-detail'
+            },
+            'sanction': {
+                'view_name': 'api:hr:sanction-detail'
+            },
+            'others_implicated': {
+                'view_name': 'api:hr:employee-detail'
+            },
+            'victims': {
+                'view_name': 'api:hr:employee-detail'
+            }
+        }
 
 
 class AcademicInstitutionSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.AcademicInstitution
+        fields = (
+            'url',
+            'id',
+            'name',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:academicinstitution-detail'
+            }
+        }
 
 
 class DegreeSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.Degree
+        fields = (
+            'url',
+            'id',
+            'name',
+            'points'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:degree-detail'
+            }
+        }
 
 
 class EmployeeHasDegreeSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = models.EmployeeHasDegree
+        fields = (
+            'url',
+            'id',
+            'employee',
+            'degree',
+            'academic_institution',
+            'ingress_year',
+            'egress_year'
+        )
+        extra_kwargs = {
+            'url': {
+                'view_name': 'api:hr:employeehasdegree-detail'
+            },
+            'employee': {
+                'view_name': 'api:hr:employee-detail'
+            },
+            'degree': {
+                'view_name': 'api:hr:degree-detail'
+            },
+            'academic_institution': {
+                'view_name': 'api:hr:academicinstitution-detail'
+            }
+        }
 
