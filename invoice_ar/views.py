34495ac432fa1a22682156b7042e2601f042f5cb
Mariano Baragiola <mariano@baragiola.com.ar>
2016-03-04 18:06:24 -0300

    ADD initial invoice_ar/views.py

--- invoice_ar/views.py
+++ invoice_ar/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from invoice import models, serializers
+
+
+class ClientARViewSet(ModelViewSet):
+    queryset = models.ClientAR.objects.all()
+    serializer_class = serializers.ClientARSerializer
+
+
+class CompanyInvoiceARViewSet(ModelViewSet):
+    queryset = models.CompanyInvoiceAR.objects.all()
+    serializer_class = serializers.CompanyInvoiceAR.objects.all()
+
