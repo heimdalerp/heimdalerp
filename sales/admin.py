49807e37c0fe0ccc36c229bee6729801cc13e5d3
mbaragiola <mariano@baragiola.com.ar>
2016-03-11 16:43:31 -0300

    ADD initial sales/admin.py

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
-# Register your models here.
+from invoice import models
+
+
+class ProductCategoryAdmin(VersionAdmin):
+    pass
+
+
+class ProductSalesAdmin(VersionAdmin):
+    pass
+
+
+class QuotationLineAdmin(VersionAdmin):
+    pass
+
+
+class QuotationAdmin(VersionAdmin):
+    pass
+
+
+class SaleLineAdmin(VersionAdmin):
+    pass
+
+
+class SaleAdmin(VersionAdmin):
+    pass
+
+
+admin.site.register(models.ProductCategory, ProductCategoryAdmin)
+admin.site.register(models.ProductSales, ProductSalesAdmin)
+admin.site.register(models.QuotationLine, QuotationLineAdmin)
+admin.site.register(models.Quotation, QuotationAdmin)
+admin.site.register(models.SaleLine, SaleLineAdmin)
+admin.site.register(models.Sale, SaleAdmin)
