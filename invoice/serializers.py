bf38b43c6ae365e3a3a673151fbc960c5dc0e0f2
Mariano Baragiola <mariano@baragiola.com.ar>
2016-06-11 20:37:01 -0300

    IMP invoice/serializers.py Further improvement of create/update for InvoiceSerializer

--- invoice/serializers.py
+++ invoice/serializers.py
@@ -471,6 +471,16 @@ class InvoiceSerializer(HyperlinkedModelSerializer):
         if number is None or number == 0:
             validated_data['number'] = 0
 
+        invoice_company = validated_data.get('invoice_company')
+        if validated_data.get('invoice_debit_account') is None:
+            validated_data['invoice_debit_account'] = (
+                invoice_company.default_invoice_debit_account
+            )
+        if validated_data.get('invoice_credit_account') is None:
+            validated_data['invoice_debit_account'] = (
+                invoice_company.default_invoice_credit_account
+            )
+
         invoice_lines_data = validated_data.pop('invoice_lines')
 
         invoice = models.Invoice.objects.create(
@@ -517,6 +527,18 @@ class InvoiceSerializer(HyperlinkedModelSerializer):
                 'invoice_date',
                 instance.invoice_date
             )
+            instance.notes = validated_data.get(
+                'notes',
+                instance.notes
+            )
+            instance.debit_account = validated_data.get(
+                'invoice_debit_account',
+                instance.invoice_debit_account
+            )
+            instance.invoice_credit_account = validated_data.get(
+                'invoice_credit_account',
+                instance.invoice_credit_account
+            )
 
             invoice_lines_data = validated_data.get('invoice_lines')
             if invoice_lines_data is not None:
@@ -547,6 +569,6 @@ class InvoiceSerializer(HyperlinkedModelSerializer):
                 instance.subtotal = subtotal
                 instance.total = total
 
-                instance.save()
+            instance.save()
 
         return instance
