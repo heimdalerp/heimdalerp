074ff6dd70facb510ad66b24707a60984e6c9e7c
Mariano Baragiola <mariano@baragiola.com.ar>
2016-07-13 18:36:33 -0300

    IMP invoice/tests

--- invoice/tests/test_serializers.py
+++ invoice/tests/test_serializers.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from contact.models import Contact
 from geo.models import Locality, Country
 from persons.models import Company
 from invoice import models
@@ -40,15 +41,16 @@ class CompanyInvoiceTestCase(APITestCase):
                 'locality': reverse('api:geo:locality-detail', args=[1]),
                 'postal_code': '3000'
             },
-            'default_invoice_debit_account': None,
-            'default_invoice_credit_account': None
+            'default_invoice_debit_account': '',
+            'default_invoice_credit_account': ''
         }
         self.response = self.client.post(url, data)
 
     def tearDown(self):
         models.CompanyInvoice.objects.filter(
-            persons_company__fantasy_name='IRONA'
+            persons_company__fantasy_name='ANORI'
         ).delete()
+        Company.objects.filter(fantasy_name='ANORI').delete()
 
     def test_create(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -99,6 +101,83 @@ class CompanyInvoiceTestCase(APITestCase):
             '3000'
         )
 
+    def test_update(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        url = reverse(
+            'api:invoice:companyinvoice-detail',
+            args=[
+                models.CompanyInvoice.objects.get(
+                    persons_company__fantasy_name='IRONA'
+                ).pk
+            ]
+        )
+        data = {
+            'persons_company': {
+                'fantasy_name': 'ANORI',
+                'legal_name': 'Zanitti-Baragiola SH',
+                'slogan': 'when face the slogan no',
+                'initiated_activities': '2015-02-03'
+            },
+            'fiscal_position': (
+                reverse('api:invoice:fiscalposition-detail', args=[2])
+            ),
+            'fiscal_address': {
+                'street_address': 'San Martín 1100',
+                'floor_number': '1',
+                'apartment_number': '2',
+                'locality': reverse('api:geo:locality-detail', args=[2]),
+                'postal_code': '2000'
+            },
+            'default_invoice_debit_account': '',
+            'default_invoice_credit_account': ''
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        obj = models.CompanyInvoice.objects.get(
+            persons_company__fantasy_name='ANORI'
+        )
+        self.assertEqual(
+            obj.persons_company.fantasy_name,
+            'ANORI'
+        )
+        self.assertEqual(
+            obj.persons_company.legal_name,
+            'Zanitti-Baragiola SH'
+        )
+        self.assertEqual(
+            obj.persons_company.slogan,
+            'when face the slogan no'
+        )
+        self.assertEqual(
+            obj.persons_company.initiated_activities,
+            date(2015, 2, 3)
+        )
+        self.assertEqual(
+            obj.fiscal_position,
+            models.FiscalPosition.objects.get(name='Do No Easy')
+        )
+        self.assertEqual(
+            obj.fiscal_address.street_address,
+            'San Martín 1100'
+        )
+        self.assertEqual(
+            obj.fiscal_address.floor_number,
+            '1'
+        )
+        self.assertEqual(
+            obj.fiscal_address.apartment_number,
+            '2'
+        )
+        self.assertEqual(
+            obj.fiscal_address.locality,
+            Locality.objects.get(pk=2)
+        )
+        self.assertEqual(
+            obj.fiscal_address.postal_code,
+            '2000'
+        )
+
 
 class ContactInvoiceTestCase(APITestCase):
     """
@@ -135,7 +214,7 @@ class ContactInvoiceTestCase(APITestCase):
                     'floor_number': '',
                     'apartment_number': '',
                     'locality': (
-                        reverse('api:geo:locality-detail', args=[1]),
+                        reverse('api:geo:locality-detail', args=[1])
                     ),
                     'postal_code': '3000'
                 }
@@ -156,8 +235,9 @@ class ContactInvoiceTestCase(APITestCase):
 
     def tearDown(self):
         models.ContactInvoice.objects.filter(
-            contact_contact__name='Tobias Riper'
+            contact_contact__name='Riper Tobias'
         ).delete()
+        Contact.objects.filter(name='Riper Tobias').delete()
 
     def test_create(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -242,4 +322,135 @@ class ContactInvoiceTestCase(APITestCase):
         self.assertEqual(
             obj.fiscal_address.postal_code,
             '3000'
+        )
+
+    def test_update(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        url = reverse(
+            'api:invoice:contactinvoice-detail',
+            args=[
+                models.ContactInvoice.objects.get(
+                    contact_contact__name='Tobias Riper'
+                ).pk
+            ]
+        )
+        data = {
+            'contact_contact': {
+                'persons_company': (
+                    reverse(
+                        'api:persons:company-detail',
+                        args=[Company.objects.get(fantasy_name='IRONA').pk]
+                    )
+                ),
+                'name': 'Riper Tobias',
+                'birth_date': '1980-09-09',
+                'born_in': reverse('api:geo:country-detail', args=[2]),
+                'phone_numbers': '123456',
+                'extra_emails': (
+                    'kek@top.com'
+                ),
+                'contact_type': 'C',
+                'home_address': {
+                    'street_address': 'San Martín 1100',
+                    'floor_number': '1',
+                    'apartment_number': '2',
+                    'locality': (
+                        reverse('api:geo:locality-detail', args=[2])
+                    ),
+                    'postal_code': '2000'
+                }
+            },
+            'legal_name': 'Riper Tobias',
+            'fiscal_position': (
+                reverse('api:invoice:fiscalposition-detail', args=[2])
+            ),
+            'fiscal_address': {
+                'street_address': 'San Martín 1100',
+                'floor_number': '1',
+                'apartment_number': '2',
+                'locality': reverse('api:geo:locality-detail', args=[2]),
+                'postal_code': '2000'
+            }
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        obj = models.ContactInvoice.objects.get(
+            contact_contact__name='Riper Tobias'
+        )
+        self.assertEqual(
+            obj.contact_contact.persons_company,
+            Company.objects.get(fantasy_name='IRONA')
+        )
+        self.assertEqual(
+            obj.contact_contact.name,
+            'Riper Tobias'
+        )
+        self.assertEqual(
+            obj.contact_contact.birth_date,
+            date(1980, 9, 9)
+        )
+        self.assertEqual(
+            obj.contact_contact.born_in,
+            Country.objects.get(pk=2)
+        )
+        self.assertEqual(
+            obj.contact_contact.phone_numbers,
+            '123456'
+        )
+        self.assertEqual(
+            obj.contact_contact.extra_emails,
+            'kek@top.com'
+        )
+        self.assertEqual(
+            obj.contact_contact.contact_type,
+            'C'
+        )
+        self.assertEqual(
+            obj.contact_contact.home_address.street_address,
+            'San Martín 1100'
+        )
+        self.assertEqual(
+            obj.contact_contact.home_address.floor_number,
+            '1'
+        )
+        self.assertEqual(
+            obj.contact_contact.home_address.apartment_number,
+            '2'
+        )
+        self.assertEqual(
+            obj.contact_contact.home_address.locality,
+            Locality.objects.get(pk=2)
+        )
+        self.assertEqual(
+            obj.contact_contact.home_address.postal_code,
+            '2000'
+        )
+        self.assertEqual(
+            obj.legal_name,
+            'Riper Tobias'
+        )
+        self.assertEqual(
+            obj.fiscal_position,
+            models.FiscalPosition.objects.get(name='Do No Easy')
+        )
+        self.assertEqual(
+            obj.fiscal_address.street_address,
+            'San Martín 1100'
+        )
+        self.assertEqual(
+            obj.fiscal_address.floor_number,
+            '1'
+        )
+        self.assertEqual(
+            obj.fiscal_address.apartment_number,
+            '2'
+        )
+        self.assertEqual(
+            obj.fiscal_address.locality,
+            Locality.objects.get(pk=2)
+        )
+        self.assertEqual(
+            obj.fiscal_address.postal_code,
+            '2000'
         )
