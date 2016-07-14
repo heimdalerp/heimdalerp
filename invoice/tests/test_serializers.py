4f402d187bd73db0013787fb2d7cab89b44cc339
Mariano Baragiola <mariano@baragiola.com.ar>
2016-07-14 18:41:34 -0300

    IMP invoice/tests

--- invoice/tests/test_serializers.py
+++ invoice/tests/test_serializers.py
@@ -619,6 +619,24 @@ class ProductTestCase(APITestCase):
             models.VAT.objects.get(name='10%')
         )
 
+    def test_update(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        obj = models.Product.objects.get(name='Do Easy')
+        url = reverse('api:invoice:product-detail', args=[obj.pk])
+        
+        data = {'current_price': -100.00}
+        response = self.client.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        obj = models.Product.objects.get(name='Do Easy')
+        self.assertEqual(obj.current_price, Decimal('100.00'))
+
+        data = {'current_price': 101.00}
+        response = self.client.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        obj = models.Product.objects.get(name='Do Easy')
+        self.assertEqual(obj.current_price, Decimal('101.00'))
+
 
 class InvoiceLineTestCase(APITestCase):
     """
@@ -670,61 +688,26 @@ class InvoiceLineTestCase(APITestCase):
         obj = models.InvoiceLine.objects.get(product__name='Do Easy')
         url = reverse('api:invoice:invoiceline-detail', args=[obj.pk])
 
-        data = {
-            'product': reverse(
-                'api:invoice:product-detail',
-                args=[models.Product.objects.get(name='Do Easy').pk]
-            ),
-            'price_sold': -100.00,
-            'discount': 0.00,
-            'quantity': 2,
-            'description': 'cardio kills gains'
-        }
-        response = self.client.put(url, data)
+        data = {'price_sold': -100.00}
+        response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        obj = models.InvoiceLine.objects.get(product__name='Do Easy')
         self.assertEqual(obj.price_sold, Decimal('100.00'))
 
-        data = {
-            'product': reverse(
-                'api:invoice:product-detail',
-                args=[models.Product.objects.get(name='Do Easy').pk]
-            ),
-            'price_sold': 101.00,
-            'discount': 0.00,
-            'quantity': 2,
-            'description': 'cardio kills gains'
-        }
-        response = self.client.put(url, data)
+        data = {'price_sold': 101.00}
+        response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         obj = models.InvoiceLine.objects.get(product__name='Do Easy')
         self.assertEqual(obj.price_sold, Decimal('101.00'))
 
-        data = {
-            'product': reverse(
-                'api:invoice:product-detail',
-                args=[models.Product.objects.get(name='Do Easy').pk]
-            ),
-            'price_sold': 100.00,
-            'discount': -0.10,
-            'quantity': 2,
-            'description': 'cardio kills gains'
-        }
-        response = self.client.put(url, data)
+        data = {'discount': -0.10}
+        response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         obj = models.InvoiceLine.objects.get(product__name='Do Easy')
         self.assertEqual(obj.discount, Decimal('0.00'))
 
-        data = {
-            'product': reverse(
-                'api:invoice:product-detail',
-                args=[models.Product.objects.get(name='Do Easy').pk]
-            ),
-            'price_sold': 100.00,
-            'discount': 0.10,
-            'quantity': 2,
-            'description': 'cardio kills gains'
-        }
-        response = self.client.put(url, data)
+        data = {'discount': 0.10}
+        response = self.client.patch(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         obj = models.InvoiceLine.objects.get(product__name='Do Easy')
         self.assertEqual(obj.discount, Decimal('0.10'))
