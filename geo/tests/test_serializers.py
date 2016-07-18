80d6b57207dc0456e2a5ba4e9094218b6b10d446
Mariano Baragiola <mariano@baragiola.com.ar>
2016-07-18 19:02:42 -0300

    IMP geo/tests

--- geo/tests/test_serializers.py
+++ geo/tests/test_serializers.py
@@ -12,8 +12,8 @@ class LocalityTestCase(APITestCase):
     """
     """
     fixtures = [
-        'persons/tests/fixtures/users.json',
-        'persons/tests/fixtures/geo.json'
+        'geo/tests/fixtures/users.json',
+        'geo/tests/fixtures/geo.json'
     ]
 
     def setUp(self):
@@ -21,10 +21,10 @@ class LocalityTestCase(APITestCase):
         self.client.force_authenticate(user=admin)
         url = reverse('api:geo:locality-list')
         data = {
-            'default_name': 'Santa Fe',
+            'default_name': 'santa fe',
             'alternative_names': [
                 {
-                    'name': 'Santa Fe',
+                    'name': 'Holy Faith',
                     'language_code': 'en'
                 },
                 {
@@ -34,15 +34,15 @@ class LocalityTestCase(APITestCase):
             ],
             'region': reverse(
                 'api:geo:region-detail',
-                args=[models.Region.objects.get(default_name='Santa Fe').pk]
+                args=[
+                    models.Region.objects.get(default_name='Entre Ríos').pk
+                ]
             )
         }  
         self.response = self.client.post(url, data)
 
     def tearDown(self):
-        models.Locality.objects.filter(
-            name='Santa Fe'
-        ).delete()
+        models.Locality.objects.filter(default_name='Santa Fe').delete()
 
     def test_create(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
@@ -50,9 +50,100 @@ class LocalityTestCase(APITestCase):
 
     def test_correctness(self):
         obj = models.Locality.objects.get(
-            name='Santa Fe'
+            default_name='santa fe'
         )
-        self.assertEqual(obj.name, 'Santa Fe')
+        self.assertEqual(obj.default_name, 'santa fe')
+        self.assertEqual(
+            obj.alternative_names.all()[0].name,
+            'Holy Faith'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[0].language_code,
+            'en'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[1].name,
+            'Santa Fe'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[1].language_code,
+            'es'
+        )
+        self.assertEqual(
+            obj.region,
+            models.Region.objects.get(default_name='Entre Ríos')
+        )
+
+    def test_update(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        url = reverse(
+            'api:geo:locality-detail',
+            args=[models.Locality.objects.get(default_name='santa fe').pk]
+        )
+        data = {
+            'default_name': 'Santa Fe',
+            'alternative_names': [
+                {
+                    'id': (
+                        models.AlternativeName.objects.get(
+                            name='Holy Faith',
+                            language_code='en'
+                        ).pk
+                    ),
+                    'name': 'Santa Fe',
+                    'language_code': 'en'
+                },
+                {
+                    'id': (
+                        models.AlternativeName.objects.get(
+                            name='Santa Fe',
+                            language_code='es'
+                        ).pk
+                    ),
+                    'name': 'Santa Fe',
+                    'language_code': 'es'
+                },
+                {
+                    'name': 'Santa Fe',
+                    'language_code': 'fr'
+                }
+            ],
+            'region': reverse(
+                'api:geo:region-detail',
+                args=[models.Region.objects.get(default_name='Santa Fe').pk]
+            )
+        }  
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        obj = models.Locality.objects.get(
+            default_name='Santa Fe'
+        )
+        self.assertEqual(obj.default_name, 'Santa Fe')
+        self.assertEqual(
+            obj.alternative_names.all()[0].name,
+            'Santa Fe'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[0].language_code,
+            'en'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[1].name,
+            'Santa Fe'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[1].language_code,
+            'es'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[2].name,
+            'Santa Fe'
+        )
+        self.assertEqual(
+            obj.alternative_names.all()[2].language_code,
+            'fr'
+        )
         self.assertEqual(
             obj.region,
             models.Region.objects.get(default_name='Santa Fe')
