23f246fa1ba64ee65791edc5aac551efa3dc647b
Mariano Baragiola <mariano@baragiola.com.ar>
2016-07-26 11:36:55 -0300

    IMP accounting/tests

--- accounting/tests/test_serializers.py
+++ accounting/tests/test_serializers.py
@@ -84,7 +84,7 @@ class AccountTestCase(APITestCase):
 
     def test_create(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(models.Ledger.objects.count(), 1)
+        self.assertEqual(models.Account.objects.count(), 3)
 
     def test_correctness(self):
         obj = models.Account.objects.get(code='1')
@@ -107,4 +107,232 @@ class AccountTestCase(APITestCase):
         self.assertEqual(
             obj.balance,
             Decimal('0.00')
+        )
+
+
+class TransactionTestCase(APITestCase):
+    """
+    """
+    fixtures = [
+        'accounting/tests/fixtures/users.json',
+        'accounting/tests/fixtures/companies.json',
+        'accounting/tests/fixtures/accounts.json'
+    ]
+
+    def setUp(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        url = reverse('api:accounting:account-list')
+        data = {
+            'amount': Decimal('100.00'),
+            'debit_account': (
+                reverse(
+                    'api:accounting:account-detail',
+                    args=[models.Account.objects.get(name='Do Easy').pk]
+                )
+            ),
+            'debit_account_balance': Decimal('100.00'),
+            'credit_account': (
+                reverse(
+                    'api:accounting:account-detail',
+                    args=[models.Account.objects.get(name='Do No Easy').pk]
+                )
+            ),
+            'credit_account_balance': Decimal('100.00'),
+        }
+        self.response = self.client.post(url, data)
+
+    def test_create(self):
+        self.assertEqual(
+            self.response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+        self.assertEqual(models.Transaction.objects.count(), 0)
+
+
+class CompanyAccountingTestCase(APITestCase):
+    """
+    """
+    fixtures = [
+        'accounting/tests/fixtures/users.json',
+        'accounting/tests/fixtures/companies.json',
+        'accounting/tests/fixtures/accounts.json'
+    ]
+
+    def setUp(self):
+        admin = User.objects.get(username='admin')
+        self.client.force_authenticate(user=admin)
+        url = reverse('api:accounting:companyaccounting-list')
+        do_easy_account = models.Account.objects.get(name='Do Easy')
+        do_no_easy_account = models.Account.objects.get(name='Do No Easy')
+        data = {
+            'persons_company': (
+                reverse(
+                    'api:persons:company-detail',
+                    args=[Company.objects.get(fantasy_name='IRONA').pk]
+                )
+            ),
+            'default_debit_account_for_cash': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_cash': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_creditcard': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_creditcard': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_debitcard': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_debitcard': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_bankaccount': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_bankaccount': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_check': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_check': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_paypal': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_paypal': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_googlewallet': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_googlewallet': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_applepay': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_applepay': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            ),
+            'default_debit_account_for_bitcoin': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_easy_account.pk]
+            ),
+            'default_credit_account_for_bitcoin': reverse(
+                    'api:accounting:account-detail',
+                    args=[do_no_easy_account.pk]
+            )
+        }
+        self.response = self.client.post(url, data)
+
+    def tearDown(self):
+        models.CompanyAccounting.objects.get().delete()
+
+    def test_create(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(models.CompanyAccounting.objects.count(), 1)
+
+    def test_correctness(self):
+        obj = models.CompanyAccounting.objects.get(
+            persons_company__fantasy_name='IRONA'
+        )
+        do_easy_account = models.Account.objects.get(name='Do Easy')
+        do_no_easy_account = models.Account.objects.get(name='Do No Easy')
+        self.assertEqual(
+            obj.persons_company,
+            Company.objects.get(fantasy_name='IRONA')
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_cash,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_cash,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_creditcard,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_creditcard,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_debitcard,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_debitcard,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_bankaccount,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_bankaccount,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_check,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_check,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_paypal,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_paypal,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_googlewallet,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_googlewallet,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_applepay,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_applepay,
+            do_no_easy_account
+        )
+        self.assertEqual(
+            obj.default_debit_account_for_bitcoin,
+            do_easy_account
+        )
+        self.assertEqual(
+            obj.default_credit_account_for_bitcoin,
+            do_no_easy_account
         )
